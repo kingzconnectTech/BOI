c5f2744a059e092a093ed1e4a74c172cb7a150fb
kingzconnectTech <kingzconnect911@gmail.com>
2026-01-03 01:45:44 +0100

    style(redis_client): use single quotes for redis password string

--- backend/redis_client.py
+++ backend/redis_client.py
@@ -6,7 +6,7 @@ import json
 REDIS_HOST = os.getenv("REDIS_HOST", "172.31.8.33")
 REDIS_PORT = int(os.getenv("REDIS_PORT", 6379))
 REDIS_DB = int(os.getenv("REDIS_DB", 0))
-REDIS_PASSWORD = os.getenv("REDIS_PASSWORD", "BoiRedis@2026!")
+REDIS_PASSWORD = os.getenv("REDIS_PASSWORD", 'BoiRedis@2026!')
 
 try:
     redis_client = redis.Redis(
